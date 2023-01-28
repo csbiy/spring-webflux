89c2418ba63bcf612c638b6e242d86d1504a8336
devcs96 <katd6@naver.com>
2023-01-28 16:45:14 +0900

    init

---
+++ src/test/java/com/greglturnquist/hackingspringbootch1/reactive/HackingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.hackingspringbootch1.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
